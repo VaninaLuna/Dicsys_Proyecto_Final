1fe2a95f9a301c2b4f0489b3e3226beb00854dab
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-12-16 15:28:53 -0300

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -24,13 +24,12 @@ CREATE TABLE `categorias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(150) NOT NULL,
   `url_img` varchar(250) DEFAULT NULL,
-  `is_activo` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 /*Data for the table `categorias` */
 
-insert  into `categorias`(`id`,`nombre`,`url_img`,`is_activo`) values (1,'Bebidas','https://www.coca-cola.com/content/dam/onexp/es/es/sustainability/in-our-products/acciones-para-contribuir-a-entornos-alimentarios-mas-saludables/our-way-forward.jpg',1),(2,'Pizzas','https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2021/03/30/6063031b90a87.r_d.1083-871-0.jpeg',1),(3,'Hamburguesas','https://cloudfront-us-east-1.images.arcpublishing.com/infobae/24P2OKC3RVEHRD3F2VKQ76XX7M.jpg',1),(4,'Tacos','https://media.istockphoto.com/id/1430849921/es/foto/orden-de-tacos-closeup.jpg?s=612x612&w=0&k=20&c=OUrSGX2P6UlyrD13G8QJ_mRQ_d5luWEm2JJRx8q1D4M=',1),(7,'Postres','https://www.unileverfoodsolutions.es/dam/global-ufs/mcos/SPAIN/calcmenu/recipes/ES-recipes/general/variedad-de-mini-postres-para-compartir/main-header.jpg',1);
+insert  into `categorias`(`id`,`nombre`,`url_img`) values (1,'Bebidas','https://www.coca-cola.com/content/dam/onexp/es/es/sustainability/in-our-products/acciones-para-contribuir-a-entornos-alimentarios-mas-saludables/our-way-forward.jpg'),(2,'Pizzas','https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2021/03/30/6063031b90a87.r_d.1083-871-0.jpeg'),(3,'Hamburguesas','https://cloudfront-us-east-1.images.arcpublishing.com/infobae/24P2OKC3RVEHRD3F2VKQ76XX7M.jpg'),(4,'Tacos','https://media.istockphoto.com/id/1430849921/es/foto/orden-de-tacos-closeup.jpg?s=612x612&w=0&k=20&c=OUrSGX2P6UlyrD13G8QJ_mRQ_d5luWEm2JJRx8q1D4M='),(7,'Postres','https://www.unileverfoodsolutions.es/dam/global-ufs/mcos/SPAIN/calcmenu/recipes/ES-recipes/general/variedad-de-mini-postres-para-compartir/main-header.jpg');
 
 /*Table structure for table `productos` */
 
@@ -44,17 +43,16 @@ CREATE TABLE `productos` (
   `stock` int(11) DEFAULT NULL,
   `precio` double DEFAULT NULL,
   `url_img` varchar(250) DEFAULT NULL,
-  `is_activo` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_productos` (`id_categoria`),
   CONSTRAINT `FK_productos` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE,
   CONSTRAINT `categorias` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 /*Data for the table `productos` */
 
-insert  into `productos`(`id`,`nombre`,`fecha_vencimiento`,`id_categoria`,`stock`,`precio`,`url_img`,`is_activo`) values (1,'Coca Cola','2024-10-05',1,120,1500,'https://f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/i/m/imageedit_3_4516158279.png',1),(2,'Fanta','2025-02-04',1,120,1350,'https://acdn.mitiendanube.com/stores/001/144/141/products/whatsapp-image-2021-08-27-at-17-12-16-4ad73445aa7845684216301110875568-640-0.jpeg',1),(3,'Margarita','2024-06-16',2,50,875,'https://hoycocino.com.ar/wp-content/uploads/2023/08/pizza-margarita.jpg',1),(4,'Cuatro Quesos','2024-12-27',2,100,1200,'https://www.hola.com/horizon/landscape/e8bb41b65869-pizzacuatroquesos-adob-t.jpg',1),(5,'Hamburguesa Clásica','2024-12-21',3,230,450,'https://bambuleburger.com/wp-content/uploads/2024/04/HAMBURGUESA-5.5.jpg',1),(6,'Hamburguesa con Bacon y Queso','2024-12-31',3,68,420,'https://images.mrcook.app/recipe-image/019150ae-aeb0-7f69-b4f0-8a87dae14972',1),(7,'Taco de Carne Asada','2024-12-31',4,100,890,'https://images.mrcook.app/recipe-image/0191822b-9e2f-7516-bccc-6323a12eda51',1),(8,'Taco de Pollo','2024-12-28',4,23,1450,'https://assets.unileversolutions.com/recipes-v2/234572.jpg',1),(22,'Brownie con Helado','2025-01-04',7,10,15000,'https://images.mrcook.app/recipe-image/01903da4-f86c-7c27-bd05-22b2b88e8d38',NULL),(24,'Cheesecake','2024-12-21',7,12,2000,'https://www.infobae.com/new-resizer/QOvWExSdTGV_-rreEP3F9U_hRpw=/arc-anglerfish-arc2-prod-infobae/public/4CJP46SGXVGXLGGMTOY4K6YPNQ.jpg',NULL);
+insert  into `productos`(`id`,`nombre`,`fecha_vencimiento`,`id_categoria`,`stock`,`precio`,`url_img`) values (1,'Coca Cola','2024-10-05',1,120,15000,'https://f2h.shop/media/catalog/product/cache/ab45d104292f1bb63d093e6be8310c97/i/m/imageedit_3_4516158279.png'),(2,'Fanta','2025-02-04',1,120,1350,'https://acdn.mitiendanube.com/stores/001/144/141/products/whatsapp-image-2021-08-27-at-17-12-16-4ad73445aa7845684216301110875568-640-0.jpeg'),(3,'Margarita','2024-06-16',2,50,875,'https://hoycocino.com.ar/wp-content/uploads/2023/08/pizza-margarita.jpg'),(4,'Cuatro Quesos','2024-12-27',2,100,1200,'https://www.hola.com/horizon/landscape/e8bb41b65869-pizzacuatroquesos-adob-t.jpg'),(5,'Hamburguesa Clásica','2024-12-21',3,230,450,'https://bambuleburger.com/wp-content/uploads/2024/04/HAMBURGUESA-5.5.jpg'),(6,'Hamburguesa con Bacon y Queso','2024-12-31',3,68,420,'https://images.mrcook.app/recipe-image/019150ae-aeb0-7f69-b4f0-8a87dae14972'),(7,'Taco de Carne Asada','2024-12-31',4,100,890,'https://images.mrcook.app/recipe-image/0191822b-9e2f-7516-bccc-6323a12eda51'),(8,'Taco de Pollo','2024-12-28',4,23,1450,'https://assets.unileversolutions.com/recipes-v2/234572.jpg'),(22,'Brownie con Helado','2025-01-04',7,10,15000,'https://images.mrcook.app/recipe-image/01903da4-f86c-7c27-bd05-22b2b88e8d38'),(24,'Cheesecake','2024-12-21',7,12,2000,'https://www.infobae.com/new-resizer/QOvWExSdTGV_-rreEP3F9U_hRpw=/arc-anglerfish-arc2-prod-infobae/public/4CJP46SGXVGXLGGMTOY4K6YPNQ.jpg'),(26,'Pizza Hawaiana','2024-12-24',2,10,2500,'https://www.eluniverso.com/resizer/v2/WZROS4TDXFGCLBRRP7K2ELZGXE.jpg?auth=76f2887d8d2a11573e1cdaa692fe3d0737e1bac7ec7ed4346c3b7ca31efdd919&width=1005&height=670&quality=75&smart=true');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
